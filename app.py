d695afa64e0223f9694dd90767dd00a9c8301f49
Sri Harini <sriharini302003@gmail.com>
2026-02-16 17:54:11 +0530

    fixed port for render

--- app.py
+++ app.py
@@ -67,5 +67,8 @@ def predict():
         return jsonify({"result": "Safe Website"})
 
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
